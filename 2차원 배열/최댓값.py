b272784616a4030c408dda7989c48387d02bc7a7
TaeWooKim-SCH <zop1234@hanmail.net>
2024-05-19 14:29:02 +0900

    add: 최댓값

---
+++ 2차원 배열/최댓값.py
@@ -0,0 +1,14 @@
+max_num = 0;
+answer_row = 0;
+answer_col = 0;
+
+for i in range(9):
+  rows = list(map(int, input().split(' ')));
+  rows_max = max(rows);
+  if (max_num < rows_max):
+    max_num = rows_max;
+    answer_row = i + 1;
+    answer_col = rows.index(max_num) + 1;
+
+print(max_num);
+print(answer_row, answer_col);
