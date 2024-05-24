3a6fd54d943fd4c115c97f6be10011a7f0f2821c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-05-24 14:45:40 +0900

    add: 색종이

--- 2차원 배열/색종이.py
+++ 2차원 배열/색종이.py
@@ -1,20 +1,17 @@
 N = int(input());
 
-init_area = 100 * N;
-
-locations = [];
-minus_area = 0;
+init_area = [[0] * 100 for _ in range(100)];
+answer_area = 0;
 
 for _ in range(N):
     x, y = list(map(int, input().split(' ')));
-    locations.append((x, y));
+    for x_i in range(10):
+        for y_i in range(10):
+            init_area[x + x_i][y + y_i] = 1;
 
-locations.sort(key = lambda x: (x[0]));
+for row in init_area:
+    answer_area += row.count(1);
 
-for i in range(len(locations) - 1):
-    x1, y1 = locations[i];
-    x2, y2 = locations[i + 1];
-    if (x1 - x2 < 10):
-        minus_area += (10 - abs(x2 - x1)) * (10 - abs(y2 - y1));
+print(answer_area);
 
-print(init_area - minus_area);
+
