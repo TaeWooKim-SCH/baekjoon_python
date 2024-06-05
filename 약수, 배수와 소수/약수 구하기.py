3fd5f13181f27a61c5ec41f5cd2da51ca638bc69
TaeWooKim-SCH <zop1234@hanmail.net>
2024-06-05 15:55:00 +0900

    add: 약수들의 합

--- 약수, 배수와 소수/약수 구하기.py
+++ 약수, 배수와 소수/약수 구하기.py
@@ -5,9 +5,8 @@ measure = [1];
 for i in range(2, N + 1):
   if (N % i == 0):
     measure.append(i);
-  if (len(measure) == K):
-    print(measure[-1]);
-    break;
 
 if (len(measure) < K):
   print(0);
+else:
+  print(measure[K - 1]);
---
+++ 약수, 배수와 소수/약수들의 합.py
@@ -0,0 +1,17 @@
+while True:
+  n = int(input());
+  perfect_num = 1;
+  answer = '{0} = 1'.format(n);
+
+  if (n == -1):
+    break;
+
+  for i in range(2, (n // 2 + 1)):
+    if (n % i == 0):
+      perfect_num += i;
+      answer += ' + {0}'.format(i);
+  
+  if (perfect_num == n):
+    print(answer);
+  else:
+    print('{0} is NOT perfect'.format(n));
