d12dc927df36e8095219980210c566008f4a900b
TaeWooKim-SCH <zop1234@hanmail.net>
2025-11-23 07:03:30 +0900

    add: 도키도키 간식드리미

---
+++ 16_스택,_큐,_덱_1/도키도키_간식드리미.py
@@ -0,0 +1,29 @@
+import sys
+
+N = int(sys.stdin.readline()); # 1 <= N <= 1,000 자연수
+num_list = list(map(int, sys.stdin.readline().split(" ")));
+
+stack = [];
+stack_size = 0;
+target = 1;
+
+while (True):
+    if (target == N):
+        print("Nice");
+        break;
+
+    is_empty_stack = stack_size == 0;
+    is_empty_num_list = len(num_list) == 0;
+        
+    if (is_empty_num_list and (num_list[0] != target)):
+        if (is_empty_stack and (stack[-1] == target)):
+            stack.pop();
+            target += 1;
+        if (is_empty_stack and (stack[-1] < num_list[0])):
+            print("Sad");
+            break;
+        stack.append(num_list.pop(0));
+        stack_size += 1;
+    elif (num_list == target):
+        target += 1;
+        num_list.pop(0);
